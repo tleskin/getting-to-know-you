e22832db0d5352c1215acc9069f4dab8e15c6a58
Thomas Leskin <tleskin@gmail.com>
2015-06-26 16:38:23 -0600

    Finish Person class in Playground

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,16 +2,45 @@
 
 import UIKit
 
+class Beer {
+    
+}
+
 class Person {
     
     // let the initializer be a string
     
     var name: String
+    var age = 0
+    
+    // question mark means it's an optional type
+    var favoriteBeer: Beer?
     
     init(name: String){
         // here
         self.name = name
+        
+    }
+//    
+//    func speak(phrase: String, repeat: Int ) {
+//        println(phrase)
+//    }
+
+    init() {
+        self.name = "No Name"
     }
 }
 
 let fred = Person(name: "Fred")
+fred.favoriteBeer
+
+
+// optional binding
+if let beer = fred.favoriteBeer {
+    println("\(fred.name)'s favorite beer is \(fred.favoriteBeer).")
+} else {
+    println("\(fred.name) has no favorite beer.")
+}
+
+//let beer = fred.favoriteBeer = Beer()
+//println("\(fred.name)'s favorite beer is \(fred.favoriteBeer).")
