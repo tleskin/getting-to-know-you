1e2b32c42fc73718192ff8090aa8be79a6e2fa75
Thomas Leskin <tleskin@gmail.com>
2015-06-26 13:16:18 -0600

    Initial Commit

---
+++ GettingToKnowYou/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GettingToKnowYou
+//
+//  Created by Thomas Leskin on 6/26/15.
+//  Copyright (c) 2015 tleskin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
